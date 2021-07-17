0037191e37878e701ca234192e0628094b2a39dd
Rohithuppalapati <rohithuppalapati77@gmail.com>
2021-07-17 21:21:27 +0530

    logical

---
+++ logical.py
@@ -0,0 +1,9 @@
+is_magician=False
+is_expert=True
+
+if is_magician and is_expert:
+    print('you are genius')
+elif is_magician and not is_expert:
+    print('atleast you are magician')
+else:
+    print('you need magic')
