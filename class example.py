8ab28f0f5ef2fdbf77bb4b23d8a31c0850d455e4
Rohithuppalapati <rohithuppalapati77@gmail.com>
2021-07-22 13:18:44 +0530

    class1

--- class example.py
+++ class example.py
@@ -8,9 +8,7 @@ cat1=Cat('linde',36)
 cat2=Cat('sun',25)
 cat3=Cat('kitty',14)
 
-
 def oldest_cat(*args):
     return max(args)
-
 
 print(f'oldet cat is {oldest_cat(cat1.age, cat2.age, cat3.age)} years old')
