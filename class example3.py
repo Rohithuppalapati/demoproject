78c86782a9452a6f34865c3634be4b355efe8642
Rohithuppalapati <rohithuppalapati77@gmail.com>
2021-07-24 20:55:35 +0530

    Time class

---
+++ class example3.py
@@ -0,0 +1,7 @@
+# Create a Time class and initialize it with hours and minutes.
+# 1. Make a method addTime which should take two time object and add them. E.g.- (2 hour and 50 min)+(1 hr and 20 min) is (4 hr and 10 min)
+# 2. Make a method displayTime which should print the time.
+# 3. Make a method DisplayMinute which should display the total minutes in the Time. E.g.- (1 hr 2 min) should display 62 minute.
+
+# answers @ https://www.codesdope.com/practice/python-your-class/
+
