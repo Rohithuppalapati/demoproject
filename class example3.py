3d70af0cbb538963296317e05c2ab6b07f1ec33b
Rohithuppalapati <rohithuppalapati77@gmail.com>
2021-07-25 19:27:42 +0530

    Time class

--- class example3.py
+++ class example3.py
@@ -5,3 +5,40 @@
 
 # answers @ https://www.codesdope.com/practice/python-your-class/
 
+class Time1():
+    def __init__(self,hours,minutes):
+        self.hours=int(hours)
+        self.minutes=int(minutes)
+
+    def displayTime(self):
+        print(f'current time is {self.hours} hours and {self.minutes} minutes')
+
+    def DisplayMinute(self):
+        m=self.hours*60 + self.minutes
+        print(f'time in minutes {m}')
+
+    def addTime(t1, t2):
+        t3 = Time1(0,0) # creating new object
+        t3.hours = t1.hours + t2.hours # sum of hours
+        t3.minutes = t1.minutes + t2.minutes # sum of minutes
+        while t3.minutes >= 60:
+            t3.hours += 1
+            t3.minutes -= 60
+        return t3
+
+
+time1=Time1('4','34')
+time2=Time1(5,40)
+
+time1.displayTime()
+print()
+time2.displayTime()
+
+print()
+time2.DisplayMinute()
+print()
+time1.DisplayMinute()
+
+print()
+#result=Time1.addTime(time1,time2)
+#print(result)
