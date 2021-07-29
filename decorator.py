96b3edd6857b472b836db30e649e7fdb6a6534b0
Rohithuppalapati <rohithuppalapati77@gmail.com>
2021-07-29 13:56:46 +0530

    decorator

--- decorator.py
+++ decorator.py
@@ -1,11 +1,12 @@
 def new_decorator(func):
     def wrapper(*args,**kwargs):
         print('*********')
-        func()
+        func(*args,**kwargs)
         print('*********')
-    return wrapper()
+    return wrapper
 
 @new_decorator
 def welcome(greeting='hello welcome to', course='python'):
     print(greeting,course)
+
 welcome()
