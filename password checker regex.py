123e48c1d59071ddb1cee39fd975b54b01526c23
Rohithuppalapati <rohithuppalapati77@gmail.com>
2021-08-08 21:07:35 +0530

    password regex

--- password checker regex.py
+++ password checker regex.py
@@ -4,8 +4,6 @@ pattern_to_search=re.compile(r"([A-Za-z0-9$%#@]{8,}[0-9])")
 
 password=input('user please enter a password ')
 
-print('invalid pwd entered')
-
 check=pattern_to_search.fullmatch(password)
 print(check)
 
